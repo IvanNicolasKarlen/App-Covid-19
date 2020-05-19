f34a01ef31478e01aac69ee8f3aafef285fdc47c
DianaAguayo <31738298+DianaAguayoUnlam@users.noreply.github.com>
2020-05-19 02:06:32 -0300

    Agrego Controlador para Usuario

---
+++ WebCovid19/Controllers/UsuarioController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCovid19.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult IndexLogueado()
+        {
+            if (Session["Email"] as string == "")
+            {
+                return RedirectToAction("Index","Home");
+            }
+            return View();
+        }
+
+
+        public ActionResult Salir()
+        {
+            Session.Clear();
+            Session.Abandon();
+            Session.RemoveAll();
+            return RedirectToAction("IndexLogueado");
+        }
+
+
+        public ActionResult Perfil()
+        {
+            Session["url"] = Request["url"];
+            if (Session["Email"] as string == "") 
+            {
+                return RedirectToAction("Login","Home");
+            }
+            return View();
+        }
+
+       
+    }
+}
